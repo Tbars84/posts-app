044cf88b12c31ce36dee20b4cf98d0b44320477d
Camilo_Tabares <camilo.tabares.84@gmail.com>
2019-02-11 14:33:48 -0500

    fixed auth domain keys

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 // CONEXION A FIREBASE
 import firebase from 'firebase'
 
--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,4 +1,4 @@
 export const AUTH_VERIF ={
-    apiKey: "AIzaSyDon8yCEGcLm2Miz9v8FhuAWHm8EvciB6I",
-    authDomain: "postapp-react.firebaseapp.com"
+    apiKey: process.env.REACT_APP_DEV_API_ID,
+    authDomain: process.env.REACT_APP_DEV_API_DOMAIN
 }
